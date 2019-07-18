84834818c6bf5edc7bb54c5a61b0dfe0922f3a71
Federico <federico.pereyra@upm.es>
2019-07-18 13:42:34 +0200

    get de archivos + get de imagen hechos

--- routes/images.js
+++ routes/images.js
@@ -1,18 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var fetch = require('node-fetch')
+var http = require('http')
+var request = require('request')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    console.log('se va a hacer un fetch')
-    fetch('http://localhost:5000/images', {method: 'GET'})
-        .then(function(result){
-          return result.json()
-        })
-        .then(function(result) {
-            console.log(result)
-            res.send(result);
-        })
+  http.get('http://localhost:5000/images', function(result) {
+    if (result.statusCode === 200) {
+      var rawData = ''
+      result.on('data', function(chunk) {
+        rawData += chunk
+      })
+      result.on('end', function() {
+        try {
+          console.log(typeof rawData)
+          res.send(rawData.trim())
+        } catch (e) {
+          console.error(e.message)
+        }
+      })
+    }
+  })
+});
+router.get('/:img', function(req, res, next) {
+  var img = req.params.img
+  console.log(img)
+  var options = {
+    url: 'http://localhost:5000/images/' + img,
+    encoding: null
+  }
+  request.get(options, function(error, response, body){
+    if (!error && response.statusCode === 200) {
+      res.set('Content-Type', 'image/jpeg')
+      res.send(body)
+    }
+  })
 });
 
 module.exports = router;
